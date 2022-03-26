91fcd5a606b3d41bd5d0eefdfedadc8020684e03
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-03-25 21:55:31 -0300

    feat: implementa o service de criacao de usuario

---
+++ src/services/CreateUserServices.ts
@@ -0,0 +1,23 @@
+import {PostgresDataSource} from '../database/data-source';
+import { User } from '../database/entity/User';
+
+interface IUserRequest {
+    name:string;
+};
+
+export class CreateUserService{
+    async execute({name}:IUserRequest):Promise<User | Error>{
+        const repo = PostgresDataSource.getRepository(User);
+        
+        if(await repo.findOneBy({name})){
+            return new Error ("The user already exist");
+        }
+
+        const user = repo.create({
+            name
+        });
+        
+        await repo.save(user);
+        return user;
+    }
+};
