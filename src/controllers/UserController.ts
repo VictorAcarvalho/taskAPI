889b8222557471d529ee0fe307a780490da6245c
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-03-25 22:12:31 -0300

    feat:implementa o UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,16 @@
+import {Request, response, Response} from 'express'; 
+import { CreateUserService } from '../services/CreateUserServices';
+
+export class UserController{
+    async handle(req:Request, res:Response){
+        const {name} = req.body;
+        const service = new CreateUserService();
+        const result = await service.execute({name});
+
+        if(result instanceof Error){
+            return response.status(401).json(result.message);
+        }
+
+        return response.status(201).json(result);
+    }
+}
