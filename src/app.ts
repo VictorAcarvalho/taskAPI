b83c538645779cd04eaf11d05224194d1a78765a
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-03-28 02:45:40 -0300

    feat: implementa o swagger

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ require('dotenv').config()
 import express from 'express';
 import morgan from 'morgan';
 import { PostgresDataSource } from './database/data-source';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from '../swagger.json';
 import { routes } from './routes';
 export const app = express();
 
@@ -15,6 +17,8 @@ PostgresDataSource
     console.log("Data source Error! ",err);
 });
 
+
+app.use("/api-doc",swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(routes);
--- src/routes.ts
+++ src/routes.ts
@@ -5,14 +5,14 @@ import { taskController } from './controllers/TaskControllers';
 const routes = Router();
 //User Routes
 routes.post("/userstore", userController.create);
-routes.get("/users",userController.list);
+routes.get("/user",userController.list);
 routes.get("/user/:user", userController.find);
 routes.put("/user/:user", userController.update);
 routes.delete("/user/:user" , userController.delete);
 
 //Task Routes
 routes.post("/task/:user",taskController.create);
-routes.get("/tasks",taskController.list);
+routes.get("/task",taskController.list);
 routes. get("/task/:task",taskController.find);
 routes.put("/task/:task", taskController.update);
 routes.delete("/task/:task",taskController.delete );
